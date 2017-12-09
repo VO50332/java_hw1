4af9ef7d5b2023319345d5591290d0e32c8910d9
VO50332 <drormiron@gmail.com>
2017-12-09 15:14:35 +0200

    package name change

--- src/hw1/LocationChangingNumberedOval.java
+++ src/hw1/LocationChangingNumberedOval.java
@@ -1,4 +1,4 @@
-package hw1_3;
+package hw1;
 
 import java.awt.*;
 
@@ -34,11 +34,15 @@ public class LocationChangingNumberedOval extends LocationChangingOval{
 		//Color numColor = new Color(1,2,3);
 		Color shapeColor = g.getColor();
 		int blue = shapeColor.getBlue();
+		int red = shapeColor.getRed();
+		int green = shapeColor.getGreen();
 		//int rgb = shapeColor.getRGB();
 		//System.out.println("rgb is" + rgb);
 		System.out.println(shapeColor + myNumber);
 		g.setColor(Color.black);
-		if (blue > 120) {
+		System.out.println(0.3*red + 0.59*green +0.11*blue);
+		System.out.println(red*red+blue*blue+green*green);
+		if (0.3*red + 0.59*green +0.11*blue < 110) {
 			g.setColor(Color.white);
 		}
 		g.drawString(myNumber,x +boundX/2,y+boundY/2);
